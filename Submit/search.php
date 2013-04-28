0df63d03c36188953fbd34ab6e610c23a0cd25f0
scit106 <scit106@gmail.com>
2013-04-28 16:33:26 -0400

    changed links wine to post link in a hidden form, got rid of update column

--- Submit/search.php
+++ Submit/search.php
@@ -33,17 +33,12 @@
 	$resulttable = str_replace("<th class=\"right\">&nbsp;</th>", "<th hidden>&nbsp;</th>", $resulttable);
 	$resulttable = str_replace("<td class=\"right\" id=\"v65-td-productListAddToCart\">", "<td hidden class=\"right\" id=\"v65-td-productListAddToCart\">", $resulttable);
 	$resulttable = str_replace("href=\"", "href=\"http://www.ballsquarefinewines.com", $resulttable); //links each row back to bsfw
-	$resulttable = str_replace(
-						"<th hidden>&nbsp;</th>"
-						, "<th hidden>&nbsp;</th>
-							<th>Select Your Wine</th>"
-						, $resulttable
-						); //to add a "use this!" column
-	$resulttable = str_replace(array("\n", "\r", "\r\n","\t","\n\t","\r\n\t","\r\t"), '', $resulttable); //removes tabs and returns for easier replaces
-	$resulttable = str_replace(	"</td></tr>"
-					, "</td><td><a href=\"placeholder\">Use This!<a></td>
-					</tr>"
-					, $resulttable);
+	$resulttable = preg_replace(
+				"/<a href=\"(.*?)\">(.*?)<\/a>/"
+				, "<form method=\"post\" action=\"submitsearch.php\">
+				<input type=\"hidden\" name=\"documentlink\" value=\"$1\">
+				<input type=\"submit\" value=\"$2\">"
+				, $resulttable);
 	echo "$resulttable";
 ?>
 </body>
