197d75dfbbd2c7f1faccf4512724f6c1aecef91b
scit106 <scit106@gmail.com>
2013-04-30 22:31:04 -0400

    Put the API search results into a table. Preliminary functionality!

--- Submit/search.php
+++ Submit/search.php
@@ -8,40 +8,36 @@
 <body>
 <div class="formalign">
 <?php
+	$apikey = "5na931xwbey79ngkupjjobm2akv7da7caa7b5rhl1asux25c";
 	$searchterm = $_POST["winename"];
-	$searchterm = str_replace(" ", "+", rtrim(ltrim($searchterm)));
-	$resultpage = file_get_contents("http://www.ballsquarefinewines.com/index.cfm?method=products.search&searchText=$searchterm"); //pulls bsfws result page
+	$searchterm = str_replace(" ", "%20", rtrim(ltrim($searchterm)));
+	//Calls snooth API with query of searchterm, 20 results
+	$resultpage = file_get_contents("http://api.snooth.com/wines?akey=$apikey&q=$searchterm&n=20"); 
+	$jsonoutput = json_decode($resultpage);
 	// str_replace(replace this, with this, in this) 
 	//substr($string, starting position (int), length (int))
 	// strpos(string, find, start)
-	// extracts the result table
-	$resulttable = substr(
-			$resultpage
-			, strpos(
-					$resultpage
-					, "<div id=\"v65-storeProductasList\">"
-					)
-			, strrpos(
-					$resultpage
-					, "<!--/Products-->"
-					)
-				- strpos(
-					$resultpage
-					, "<div id=\"v65-storeProductasList\">"
-					)
-				) ;
-	$resulttable = ltrim(str_replace("id=\"v65-storeProductasList\"", "", $resulttable));
-	$resulttable = str_replace("<th class=\"right\">&nbsp;</th>", "<th hidden>&nbsp;</th>", $resulttable);
-	$resulttable = str_replace("<td class=\"right\" id=\"v65-td-productListAddToCart\">", "<td hidden class=\"right\" id=\"v65-td-productListAddToCart\">", $resulttable);
-	$resulttable = str_replace("href=\"", "href=\"http://www.ballsquarefinewines.com", $resulttable); //links each row back to bsfw
-	$resulttable = preg_replace(
-				"/<a href=\"(.*?)\">(.*?)<\/a>/"
-				, "<form method=\"post\" action=\"editdetail.php\">
-				<input type=\"hidden\" name=\"documentlink\" value=\"$1\">
-				<input class=\"btn btn-link\" type=\"submit\" value=\"$2\">"
-				, $resulttable);
-	$resulttable = str_replace("<table", "<table class=\"table table-hover\"", $resulttable);
-	echo "$resulttable";
+?>
+<table class="table table-hover">
+	<tr>
+		<th>Name</th>
+		<th>Style</th>
+		<th>Type</th>
+		<th>Winery</th>
+		<th>Region</th>
+		<th>Price</th>
+		<th hidden>External ID</th>
+	</tr>
+<?php
+foreach ($jsonoutput->wines as $result) {
+	echo "<tr><td>{$result->name}</td>
+	<td>{$result->varietal}</td>
+	<td>{$result->type}</td>
+	<td>{$result->winery}</td>
+	<td>{$result->region}</td>
+	<td>{$result->price}</td>
+	<td hidden>{$result->code}</td></tr>";
+}
 ?>
 <p>Don't See Your Wine Listed?<a href="editdetail.php">Click Here</a> To Enter It Manually</p>
 </div>
