2200414da5758a3b0babcd82cafb91006605866a
scit106 <scit106@gmail.com>
2014-02-22 13:39:30 -0500

    fixed some links

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
 	<div class = "centered width-restricted">
 		<h1>Welcome!</h1>
 		<h3>Please Select An Action</h3>
-		<h3><a href='/vote'>[Vote]</a></h3>
-		<h3><a href="/submit">[Submit A Wine]</a></h3>
+		<h3><a href='vote'>[Vote]</a></h3>
+		<h3><a href="submit">[Submit A Wine]</a></h3>
 		<?php if (date('dGi') >= '230430'): ?>
 			<h3><a href="results.php">[View Results]</a></h3>
 		<?php endif; ?>
